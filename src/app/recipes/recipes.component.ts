6bd49db4173e76bf1ef596c25c2069f4c13f8b2b
Ross <rostikk@gmail.com>
2017-09-05 16:34:55 +0300

    changed eventEmitter to Subject subscription

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,5 +1,4 @@
-import {Component, OnInit, Input} from '@angular/core';
-import {Recipe} from './recipe.model';
+import {Component, OnInit} from '@angular/core';
 import {RecipeService} from './recipe.service';
 
 @Component({
@@ -9,15 +8,9 @@ import {RecipeService} from './recipe.service';
   providers: [RecipeService]
 })
 export class RecipesComponent implements OnInit {
-  @Input() selectedRecipe: Recipe;
-  constructor(private recipeService: RecipeService) { }
+  constructor() { }
 
   ngOnInit() {
-    this.recipeService.recipeSelected.subscribe(
-      (recipe: Recipe) =>  {
-        this.selectedRecipe = recipe;
-      }
-    );
   }
 
 }
